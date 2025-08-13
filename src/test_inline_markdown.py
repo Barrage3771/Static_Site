946bea6854b2ae14a0afd917e6497b00f19ed348
barrage3771 <barrage3771@gmail.com>
2025-08-12 20:57:18 -0400

    starting to add it all together

--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -20,3 +20,41 @@ class TestInlineMarkdown(unittest.TestCase):
             ],
             new_nodes,
         )
+        
+    def test_empty_split_images(self):
+        node = TextNode(
+            "This is a text with no image at all.",
+            TextType.TEXT
+        )
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(
+            [TextNode("This is a text with no image at all.", TextType.TEXT)],
+            new_nodes
+        )
+    
+    def test_split_links(self):
+        node = TextNode(
+            "This is text with an [link](www.example.com) and another [secondlink](www.anotherexample.com)",
+            TextType.TEXT
+        )
+        new_nodes = split_nodes_link([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ",TextType.TEXT),
+                TextNode("link", TextType.LINK, "www.example.com"),
+                TextNode(" and another ", TextType.TEXT),
+                TextNode("secondlink", TextType.LINK, "www.anotherexample.com")
+            ],
+            new_nodes
+        )
+        
+    def test_empty_links(self):
+        node = TextNode(
+            "This is a text with no link at all.",
+            TextType.TEXT
+        )
+        new_nodes = split_nodes_link([node])
+        self.assertListEqual(
+            [TextNode("This is a text with no link at all.", TextType.TEXT)],
+            new_nodes
+        )
---
+++ src/text_to_textnodes.py
@@ -0,0 +1,7 @@
+from htmlnode import *
+from textnode import *
+from inline_markdown import *
+from split_delimiter import *
+
+def text_to_textnodes(text):
+    
