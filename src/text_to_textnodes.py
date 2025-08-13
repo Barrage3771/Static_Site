adcd75c9a26d34d1956fd966fcea14507df9678a
Buns <barrage3771@gmail.com>
2025-08-13 13:13:13 -0400

    got it all working and added 2 tests, will finish this week.

--- src/text_to_textnodes.py
+++ src/text_to_textnodes.py
@@ -4,4 +4,16 @@ from inline_markdown import *
 from split_delimiter import *
 
 def text_to_textnodes(text):
-    
+    new_nodes = []
+    delimiters = [
+        ("**", TextType.BOLD),
+        ("__", TextType.ITALIC),
+        ("`", TextType.CODE),
+    ]
+    node = TextNode(text, TextType.TEXT)
+    curr_nodes = [node]
+    for delimiter, text_type in delimiters:
+        curr_nodes = split_nodes_delimiter(curr_nodes, delimiter, text_type)
+    curr_nodes = split_nodes_link(curr_nodes)
+    curr_nodes = split_nodes_image(curr_nodes)
+    return curr_nodes
