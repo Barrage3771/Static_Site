ff9c6ea82c94d300173b78121445c729b13d6aab
Buns <barrage3771@gmail.com>
2025-08-14 20:29:02 -0400

    Still want to make more tests for markdown_to_blocks but it is fully working.

--- src/test_markdown_to_blocks.py
+++ src/test_markdown_to_blocks.py
@@ -6,14 +6,14 @@ class TestMarkdownToBlocks(unittest.TestCase):
 
     def test_markdown_to_blocks(self):
         md = """
-    This is **bolded** paragraph
+This is **bolded** paragraph
 
-    This is another paragraph with _italic_ text and `code` here
-    This is the same paragraph on a new line
+This is another paragraph with _italic_ text and `code` here
+This is the same paragraph on a new line
 
-    - This is a list
-    - with items
-    """
+- This is a list
+- with items
+            """
         blocks = markdown_to_blocks(md)
         self.assertEqual(
             blocks,
